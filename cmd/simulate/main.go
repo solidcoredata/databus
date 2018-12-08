790a85322927e5ea854cd2cdf07fb825745a3742
Daniel Theophanes <daniel.theophanes@solidcoredata.com>
2018-12-08 10:45:53 -0800

    cmd/simulate: first compile

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	fmt.Println("simulate data bus")
+	a := &app{}
+	http.ListenAndServe(":8080", a)
 }
+
+type app struct{}
+
+func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("I'm alive!"))
+}
+
