8e1254656c759df06e521172ce6f72ed68527b56
Daniel Theophanes <daniel.theophanes@solidcoredata.com>
2018-12-08 13:59:02 -0800

    cmd/simulate: add busnode

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -17,3 +17,59 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("I'm alive!"))
 }
 
+type Property struct {
+	Name     string
+	Type     string
+	Optional bool
+}
+
+type KV = map[string]interface{}
+
+type Role struct {
+	Name       string
+	Properties []Property
+	Fields     []KV
+}
+
+type BusNode struct {
+	Roles []Role
+}
+
+func setupUINode() *BusNode {
+	return &BusNode{
+		Roles: []Role{
+			{
+				Name: "props1",
+				Properties: []Property{
+					{Name: "hide_new", Type: "bool"},
+					{Name: "hide_edit", Type: "bool"},
+					{Name: "hide_delete", Type: "bool"},
+				},
+				Fields: []KV{
+					{"hide_new": false},
+					{"hide_edit": false},
+					{"hide_delete": false},
+				},
+			},
+			{
+				Name: "params",
+				Properties: []Property{
+					{Name: "name", Type: "text"},
+				},
+				Fields: []KV{
+					{"name": "title_contains"},
+				},
+			},
+			{
+				Name: "arity",
+				Properties: []Property{
+					{Name: "name", Type: "text"},
+				},
+				Fields: []KV{
+					{"name": "title"},
+					{"name": "author"},
+				},
+			},
+		},
+	}
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,19 @@
 module solidcoredata.org/src/databus
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/bmatcuk/doublestar v1.1.1 // indirect
+	github.com/cortesi/modd v0.0.0-20181205233418-8d0c080f2ef8 // indirect
+	github.com/cortesi/moddwatch v0.0.0-20181112195910-5f01cc16f2a5 // indirect
+	github.com/cortesi/termlog v0.0.0-20171116205515-87cefd5ac843 // indirect
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
+	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
+	golang.org/x/net v0.0.0-20181207154023-610586996380 // indirect
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	mvdan.cc/sh v2.6.2+incompatible // indirect
+)
