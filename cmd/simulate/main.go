b39d821ff4e970ac32e8753f67170311a2f0ac51
Daniel Theophanes <daniel.theophanes@solidcoredata.com>
2018-12-13 16:43:14 -0800

    cmd/simulate: verify bindings

--- cmd/simulate/main.go
+++ cmd/simulate/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,10 +10,15 @@ func main() {
 	fmt.Println("simulate data bus")
 	uinode := setupUINode()
 	datanode := setupDataNode()
-	bind := &Bind{Together: []*BusNode{uinode, datanode}}
+	bind := &Bind{Name: "books", Together: []*BusNode{uinode, datanode}}
 	a := &app{
 		Bind: []*Bind{bind},
 	}
+	err := a.Validate()
+	if err != nil {
+		log.Print(err)
+	}
+
 	http.ListenAndServe(":8080", a)
 }
 
@@ -20,6 +26,16 @@ type app struct {
 	Bind []*Bind
 }
 
+func (a *app) Validate() error {
+	for _, b := range a.Bind {
+		err := b.Validate()
+		if err != nil {
+			return err
+		}
+
+	}
+	return nil
+}
 func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, b := range a.Bind {
 		for _, bn := range b.Together {
@@ -33,11 +49,70 @@ func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	w.Write([]byte("I'm alive!"))
+	fmt.Fprintf(w, "I'm alive!\nerrs: %v\n", a.Validate())
 }
 
 type Bind struct {
+	Name     string
 	Together []*BusNode
+
+	errors []error
+}
+
+func (b *Bind) errorf(f string, v ...interface{}) {
+	e := fmt.Errorf(f, v...)
+	b.errors = append(b.errors, e)
+}
+func (b *Bind) Err() error {
+	if len(b.errors) == 0 {
+		return nil
+	}
+	return fmt.Errorf("%q", b.errors)
+}
+
+func (b *Bind) Validate() error {
+	for _, bn := range b.Together {
+		bn.roleName = make(map[string]*Role, len(bn.Roles))
+		for ri := range bn.Roles {
+			r := &bn.Roles[ri]
+			bn.roleName[r.Name] = r
+			if len(r.Bind) == 0 {
+				continue
+			}
+			r.fieldBind = make(map[interface{}]bool, len(r.Fields))
+			for _, f := range r.Fields {
+				fv, ok := f[r.Bind]
+				if !ok {
+					continue
+				}
+				r.fieldBind[fv] = true
+			}
+		}
+	}
+	for bni, bn := range b.Together {
+		if bni+1 >= len(b.Together) {
+			continue
+		}
+		next := b.Together[bni+1]
+		for _, r := range bn.Roles {
+			if len(r.Bind) == 0 {
+				continue
+			}
+			nextRole, ok := next.roleName[r.Name]
+			if !ok {
+				b.errorf("Unable to find role %q when binding types %s to %s", r.Name, bn.Type, next.Type)
+				continue
+			}
+			for fb := range r.fieldBind {
+				if !nextRole.fieldBind[fb] {
+					b.errorf("Node Type %s Role %q: missing bound field %v", bn.Type, r.Name, fb)
+					continue
+				}
+			}
+		}
+
+	}
+	return b.Err()
 }
 
 type Property struct {
@@ -52,11 +127,16 @@ type Role struct {
 	Name       string
 	Properties []Property
 	Fields     []KV
+	Bind       string
+
+	fieldBind map[interface{}]bool
 }
 
 type BusNode struct {
 	Type  string
 	Roles []Role
+
+	roleName map[string]*Role
 }
 
 func setupUINode() *BusNode {
@@ -84,6 +164,7 @@ func setupUINode() *BusNode {
 				Fields: []KV{
 					{"name": "title_contains"},
 				},
+				Bind: "name",
 			},
 			{
 				Name: "arity",
@@ -96,6 +177,7 @@ func setupUINode() *BusNode {
 					{"name": "title", "Display": "Book Title", "Width": 200},
 					{"name": "author", "Display": "Book Author", "Width": 100},
 				},
+				Bind: "name",
 			},
 		},
 	}
@@ -122,6 +204,7 @@ func setupDataNode() *BusNode {
 				Fields: []KV{
 					{"name": "title_contains"},
 				},
+				Bind: "name",
 			},
 			{
 				Name: "arity",
@@ -132,6 +215,7 @@ func setupDataNode() *BusNode {
 					{"name": "title"},
 					{"name": "author"},
 				},
+				Bind: "name",
 			},
 		},
 	}
