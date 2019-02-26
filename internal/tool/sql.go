ff6b98b3958be1608a17503c295892c909f52823
Daniel Theophanes <daniel.theophanes@solidcoredata.com>
2019-02-26 05:43:08 -0800

    internal/tool: first steps to output SQL

--- internal/tool/sql.go
+++ internal/tool/sql.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"sort"
 
 	"solidcoredata.org/src/databus/bus"
 )
@@ -12,11 +13,17 @@ var _ bus.RunStart = &SQLGenerate{}
 
 type SQLGenerate struct{}
 
+const (
+	typeSQLDatabase = "solidcoredata.org/t/db/database"
+	typeSQLTable    = "solidcoredata.org/t/db/table"
+)
+
 func (s *SQLGenerate) NodeTypes(ctx context.Context, header *bus.CallHeader, request *bus.CallNodeTypesRequest) (*bus.CallNodeTypesResponse, error) {
 	return &bus.CallNodeTypesResponse{
 		CallVersion: 1,
 		NodeTypes: []string{
-			"solidcoredata.org/type/sql/table",
+			typeSQLDatabase,
+			typeSQLTable,
 		},
 	}, nil
 }
@@ -28,7 +35,10 @@ func (s *SQLGenerate) Run(ctx context.Context, header *bus.CallHeader, request *
 	default:
 		return nil, fmt.Errorf("unknown SQL variant type %q", variant)
 	case "postgres":
-		s.postgres(c, buf)
+		err := s.postgres(c, buf)
+		if err != nil {
+			return nil, err
+		}
 	}
 	err := outputFile(ctx, request.Root, "schema.sql", buf.Bytes())
 	if err != nil {
@@ -39,7 +49,44 @@ func (s *SQLGenerate) Run(ctx context.Context, header *bus.CallHeader, request *
 	}, nil
 }
 
-func (s *SQLGenerate) postgres(b *bus.Bus, buf *bytes.Buffer) {
-	// TODO(daniel.theophanes): read properties and create schema, setup database, create tables, sort output by dependency order.
-	buf.Write([]byte("-- This is pretend content."))
+func (s *SQLGenerate) postgres(b *bus.Bus, buf *bytes.Buffer) error {
+	nodes := b.Nodes
+	sort.Slice(nodes, func(i, j int) bool {
+		a, b := nodes[i], nodes[j]
+		if a.Type == b.Type {
+			return a.Name < b.Name
+		}
+		return a.Type < b.Type
+	})
+	w := func(s string, v ...interface{}) {
+		fmt.Fprintf(buf, s, v...)
+	}
+	for _, n := range nodes {
+		switch n.Type {
+		default:
+			return fmt.Errorf("unknown type: %q", n.Type)
+		case typeSQLDatabase:
+			prop := n.Role("prop").Fields[0]
+			name := prop.Value("name").(string)
+			w("create database %[1]s;\nset database = %[1]s;\n\n", name)
+		case typeSQLTable:
+			prop := n.Role("prop").Fields[0]
+			name := prop.Value("name")
+			db := prop.Value("database").(*bus.Node)
+			_ = db
+
+			sch := n.Role("schema")
+			w("create table %s (", name)
+			for i, f := range sch.Fields {
+				if i != 0 {
+					w(",")
+				}
+				fname := f.Value("name").(string)
+				ftype := f.Value("type").(string)
+				w("\n\t%s %s", fname, ftype)
+			}
+			w("\n);\n")
+		}
+	}
+	return nil
 }
--- internal/tool/testdata/library/output/schema.sql
+++ internal/tool/testdata/library/output/schema.sql
@@ -1,1 +1,13 @@
--- This is pretend content.
+create database library;
+set database = library;
+
+create table book (
+	id int,
+	name text,
+	genre int,
+	page_count int
+);
+create table genre (
+	id int,
+	name text
+);
